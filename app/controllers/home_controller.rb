dfac68122269017d1be895ce9342d317f17621ba
vfseanm@gmail.com <vfseanm@gmail.com>
2013-02-13 13:58:43 -0500

    Got a test controller method working!

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+      logger.info("testing!")
+      puts("testing")
+  end
+  def join_event
+      logger.info("******** Joining Event!**********")
   end
 end
