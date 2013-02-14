2b8e79548a02b7f2b8335c7c9d3918d2be6d1540
vfseanm@gmail.com <vfseanm@gmail.com>
2013-02-13 21:02:35 -0500

    experimental ajax call

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,4 +6,11 @@ class HomeController < ApplicationController
   def join_event
       logger.info("******** Joining Event!**********")
   end
+  def insert_subject
+      logger.info("****** Inserting Subject! ******")
+      #// call ashwin's db stuff
+      enrolled = params['enrolled']
+      logger.info(enrolled)
+      #redirect_to index
+   end
 end
