73951fe3d6c182af004e94c2bf1747bdd8363adc
Dan Deng <dan.deng@duke.edu>
2013-03-25 21:59:40 -0400

    demo

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,10 +4,10 @@
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // the compiled file.
 //
-require jquery
-require jquery_ujs
-require twitter/bootstrap
-require_tree .
+//=require jquery
+//=require jquery_ujs
+//=require twitter/bootstrap
+//=require_tree .
 
 
 $(document).ready(function(){
